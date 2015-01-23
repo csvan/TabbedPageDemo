a484fc475ab6b1451462620248b65eda5ec60a46
Christopher Svanefalk <christopher.svanefalk@gmail.com>
2015-01-23 10:32:19 +0100

    Initial

---
+++ TabbedPageDemo/MainPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TabbedPageDemo
+{
+	public class MainPage: TabbedPage
+	{
+		static MainPage singleton;
+
+		public static MainPage GetInstance ()
+		{
+			if (singleton == null) {
+				singleton = new MainPage ();
+			}
+			return singleton;
+		}
+
+		MainPage ()
+		{
+
+			Title = "MyNavPage";
+
+			ItemsSource = new List<String> {
+				"Locality",
+				"Nearby",
+				"Sundays",
+				"About"
+			};
+
+			PropertyChanged += (object sender, System.ComponentModel.PropertyChangedEventArgs e) => {
+				if (e.PropertyName.Equals ("SelectedItem")) {
+					Debug.WriteLine (SelectedItem);
+				}
+			};
+		}
+	}
+}
+
